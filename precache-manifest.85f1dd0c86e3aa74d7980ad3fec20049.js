e1f108f64370abf5535e86b8b01a10d5a9c6dad6
Ganesh <ganeshkrishnans94@gmail.com>
2020-01-27 01:50:30 -0800

    Updates

---
+++ precache-manifest.85f1dd0c86e3aa74d7980ad3fec20049.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5001f7aa099fb520dd17f3437595d50f",
+    "url": "/llama-app/index.html"
+  },
+  {
+    "revision": "00d1c2e370f322485f94",
+    "url": "/llama-app/static/css/main.9ffb8a4a.chunk.css"
+  },
+  {
+    "revision": "ce690920981187ba4a8f",
+    "url": "/llama-app/static/js/2.c9fb14e0.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/llama-app/static/js/2.c9fb14e0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "00d1c2e370f322485f94",
+    "url": "/llama-app/static/js/main.98803fbc.chunk.js"
+  },
+  {
+    "revision": "e029d4176baa637ee55e",
+    "url": "/llama-app/static/js/runtime-main.7a35941b.js"
+  }
+]);
